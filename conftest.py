3b6852115bb23fdee3c0ad9a915b1a6be00b047f
Master_Chief <anfefu1@gmail.com>
2026-08-24 18:28:26 -0500

    ui_page fixture generator created

--- conftest.py
+++ conftest.py
@@ -5,6 +5,8 @@
 
 import pytest
 import os
+from playwright.sync_api import Page
+from typing import Generator
 
 
 # ----------------------------------------------------------------------
@@ -21,10 +23,30 @@ def browser_context_args(browser_context_args: dict) -> dict:
     
     return {
         # **: takes all the existing key-value pairs from the 
-        # default browser_context_args dict and modifys it into
+        # default browser_context_args dict and modifies it into
         # a new dict that I'm creating.
         # Add custom settings without losing the original data
         **browser_context_args,
         "viewport": {"width":1920, "height":1080},
         "base_url": {base_url}
     }
+
+# ----------------------------------------------------------------------
+# 2. CUSTOM UI PAGE WRAPPER
+# ----------------------------------------------------------------------
+
+# Custom fixture that wraps  the default Playwright page.
+# It automatically navigates to the base URL before the test starts,
+# keeping our test files DRY
+@pytest.fixture(scope='function')
+def ui_page(page:Page) -> Generator:
+    # Setup: Go to the homepage before the test begins
+    page.goto('/')
+    # yield hands control over to the actual test function
+    yield page
+    
+    # Yield encapsulates the setup and teardown logic into a single function
+    # Yield pauses the fixture, allows the test to run and then,
+    # the cleanup happens and don't leave any garbage when the test finishes.
+    
+    
