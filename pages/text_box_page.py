ef8a47c74d56b2fa47d37bc831fb07db00f7ad66
Master_Chief <anfefu1@gmail.com>
2026-09-01 22:05:12 -0500

    text_box_page finished

--- pages/text_box_page.py
+++ pages/text_box_page.py
@@ -16,7 +16,9 @@ class TextBoxPage(BasePage):
         self.text_box_option = self.page.get_by_role('link', name='Text Box')
         self.submit_button = self.page.get_by_role('button', name='Submit')
         self.full_name = self.page.get_by_role('textbox', name='Full Name')
-        
+        self.email = self.page.get_by_role('textbox', name='Email')
+        self.current_address = self.page.get_by_role('textbox', name='Current Address')
+        self.permanent_address = self.page.get_by_role('textbox', name='Permanent Address')
         
         
     # Actions (Methods)
@@ -25,16 +27,16 @@ class TextBoxPage(BasePage):
         self.click_element(self.text_box_option)
         
     def fill_full_name(self):
-        self.fill_text()
+        self.fill_text(self.full_name)
         
     def fill_email(self):
-        self.fill_text()
+        self.fill_text(self.email)
         
     def fill_current_address(self):
-        self.fill_text()
+        self.fill_text(self.current_address)
         
     def fill_permanent_address(self):
-        self.fill_text()
+        self.fill_text(self.permanent_address)
         
     def click_submit_button(self):
         self.click_element(self.submit_button)
